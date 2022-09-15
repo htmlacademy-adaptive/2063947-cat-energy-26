4cc8a76181430e0b54ece6f053c38e0be8b8895c
Inna Romanets <romanets_inna@outlook.com>
2022-09-15 21:57:20 +0300

    ДЗ8

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
-import del from 'del';
+import { deleteAsync } from 'del';
 import htmlmin from 'gulp-htmlmin';
 import squoosh from 'gulp-libsquoosh';
 import rename from 'gulp-rename';
@@ -28,10 +28,77 @@ export const styles = () => {
 
 // HTML
 
-export const html = () => {
+const html = () => {
   return gulp.src("source/*.html")
   .pipe(htmlmin({collapseWhitespace: true}))
   .pipe(gulp.dest("build"));
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/*.js")
+  .pipe(terser())
+  .pipe(gulp.dest("build/js"));
+}
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(squoosh())
+  .pipe(gulp.dest("build/img"));
+}
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(gulp.dest("build/img"));
+}
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(squoosh({webp: {}}))
+  .pipe(gulp.dest("build/img"));
+}
+
+// SVG - оптимизировать не надо? Т.к. у меня уже все svg включены в спрайт...
+
+/*const svg = () => {
+  return gulp.src("source/img/*.svg")
+  .pipe(svgo())
+  .pipe(gulp.dest("build/img"));
+}*/
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/*.svg")
+  .pipe(svgo())
+  .pipe(svgstore({inlineSvg: true}))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+}
+
+// Copy
+
+const copy = (done) => {
+  return gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"))
+  done();
+}
+
+// Clean - не срабатывает
+
+const clean = () => {
+  return { deleteAsync }("build");
+}
 
 // Server
 
@@ -47,14 +114,31 @@ const server = (done) => {
   done();
 }
 
-// Watcher
+// Watcher - нужно ли сюда дописать scripts?
 
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    svg,
+    sprite,
+    createWebp
+  ),
+);
+
+
 
 export default gulp.series(
-  styles, server, watcher
+  html, scripts, optimizeImages, createWebp, sprite, copy, clean, styles, server, watcher
 );
